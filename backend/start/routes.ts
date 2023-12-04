8d5a1acfa7a629d3c074a66b8d00e8cb22c85809
Adam Elaoumari <adam@admlbs.fr>
2023-12-04 14:48:08 +0100

    Started ally config with google auth

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -21,5 +21,40 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hello: 'worlds' }
+  return { hello: 'world' }
+})
+
+Route.get('/google/redirect', async ({ ally }) => {
+  return ally.use('google').redirect()
+})
+
+Route.get('/google/callback', async ({ ally }) => {
+  const google = ally.use('google')
+
+  /**
+   * User has explicitly denied the login request
+   */
+  if (google.accessDenied()) {
+    return 'Access was denied'
+  }
+
+  /**
+   * Unable to verify the CSRF state
+   */
+  if (google.stateMisMatch()) {
+    return 'Request expired. Retry again'
+  }
+
+  /**
+   * There was an unknown error during the redirect
+   */
+  if (google.hasError()) {
+    return google.getError()
+  }
+
+  /**
+   * Finally, access the user
+   */
+  const user = await google.user()
+  return user.token
 })
