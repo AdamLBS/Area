1fcea3552abf6a1378c3c45c9f38729739debf65
Ramdane Martin <martin.ramdane@epitech.eu>
2023-12-04 11:41:43 +0100

    feat(Svelte): added POC

---
+++ svelte-app/app/components/Home.svelte
@@ -0,0 +1,26 @@
+<page>
+    <actionBar title="Home" />
+    <gridLayout>
+        <label class="info">
+            <formattedString>
+                <span text=" {message}" />
+            </formattedString>
+        </label>
+    </gridLayout>
+</page>
+
+<script lang="ts">
+    let message: string = "Bonjour Ramdane Martin"
+</script>
+
+<style>
+    .info .fas {
+        color: #3A53FF;
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
