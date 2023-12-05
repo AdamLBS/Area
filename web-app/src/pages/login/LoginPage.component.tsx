76962f5f85d0f8069d31bd67ea00939f611ee320
Ramdane Martin <martin.ramdane@epitech.eu>
2023-12-05 17:00:28 +0100

    feat(LoginPage): init styles

--- web-app/src/pages/login/LoginPage.component.tsx
+++ web-app/src/pages/login/LoginPage.component.tsx
@@ -1,11 +1,22 @@
-import { Button } from '@/components/ui';
+'use client';
 import React, { memo } from 'react';
+import {
+  LeftContainer,
+  PageContainer,
+  RightContainer,
+} from './LoginPage.style';
+import { PrimaryDefault } from '@/lib/ui/design-system';
 
 const Login = () => {
   return (
-    <div>
-      <Button>Login</Button>
-    </div>
+    <PageContainer>
+      <LeftContainer>
+        <PrimaryDefault>Hello</PrimaryDefault>
+      </LeftContainer>
+      <RightContainer>
+        <PrimaryDefault>Hello</PrimaryDefault>
+      </RightContainer>
+    </PageContainer>
   );
 };
 
---
+++ web-app/src/pages/login/LoginPage.style.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const LeftContainer = styled.div`
+  flex: 1;
+  background-color: #18181b;
+  padding: 40px;
+`;
+
+export const RightContainer = styled.div`
+  flex: 1;
+  padding: 40px;
+`;
