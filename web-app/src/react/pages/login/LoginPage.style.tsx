52b8aaf0950fba8eba2ae760b970f9200779e3c0
Ramdane Martin <martin.ramdane@epitech.eu>
2023-12-06 10:14:03 +0100

    feat(WepApp): adde body of 100vw et vh

--- web-app/src/react/pages/login/LoginPage.style.tsx
+++ web-app/src/react/pages/login/LoginPage.style.tsx
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const PageContainer = styled.div`
   display: flex;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 `;
 
 export const LeftContainer = styled.div`
