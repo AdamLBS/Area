d952b5f8f42459c73d2ee9fb083fa725b0ef889c
Luluri <lucas.rizzuto@epitech.eu>
2023-12-11 14:51:32 +0100

    update: add Loginform folder and files

---
+++ web-app/src/react/pages/login/LoginForm/LoginFrom.component.tsx
@@ -0,0 +1,71 @@
+'use client';
+import React, { memo, useCallback } from 'react';
+import { FormContainer } from './LoginForm.style';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Email is required' }),
+  password: z.string().min(1, { message: 'Password is required' }),
+});
+
+const LoginFormComponent = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  type FormValues = z.infer<typeof formSchema>;
+
+  const onSubmit = useCallback((values: FormValues) => {
+    // Do something with the form values.
+    console.log(values.email, values.password);
+  }, []);
+
+  return (
+    <Form {...form}>
+      <FormContainer onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="name@exemple.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" placeholder="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Sign In</Button>
+      </FormContainer>
+    </Form>
+  );
+};
+
+export const LoginForm = memo(LoginFormComponent);
