dc957dc79642963ac14ee34fbad949b4fa398caf
Adam Elaoumari <adam@admlbs.fr>
2023-12-11 16:35:50 +0100

    Fix: fixed warning

--- mobile/lib/pages/login.dart
+++ mobile/lib/pages/login.dart
@@ -142,7 +142,9 @@ class _LoginPageState extends State<LoginPage> {
                     try {
                       await logUserIn(
                           emailController.text, passController.text);
-                      Navigator.of(context).pushReplacementNamed("/home");
+                          if (context.mounted) {
+                            Navigator.of(context).pushReplacementNamed("/home");
+                          }
                     } catch (e) {
                       print(e);
                       setState(() {
