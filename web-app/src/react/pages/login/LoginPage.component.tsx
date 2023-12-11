7ae71367f6a46a31eb37ddff28cfde5c0e65711d
Luluri <lucas.rizzuto@epitech.eu>
2023-12-11 14:22:10 +0100

    feat(webapp): add checking error  for login page

--- web-app/src/react/pages/login/LoginPage.component.tsx
+++ web-app/src/react/pages/login/LoginPage.component.tsx
@@ -11,10 +11,25 @@ import {
   Subtitle,
   Title,
 } from './LoginPage.style';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
 import { ColumnLayout } from '@/lib/ui/design-system';
 import { useRouter } from 'next/navigation';
 import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui';
+import { Button } from '@/components/ui/button';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Email is required' }),
+  password: z.string().min(1, { message: 'Password is required' }),
+});
 
 const Login = () => {
   const router = useRouter();
@@ -23,6 +38,20 @@ const Login = () => {
     router.push('/register');
   }, [router]);
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  type FormValues = z.infer<typeof formSchema>;
+
+  const onSubmit = useCallback((values: FormValues) => {
+    // Do something with the form values.
+    console.log(values.email, values.password);
+  }, []);
   return (
     <PageContainer>
       <ColumnLayout />
@@ -35,9 +64,48 @@ const Login = () => {
               <Subtitle>Enter your account access below to sign in</Subtitle>
             </HeaderContainer>
             <InputsContainer>
-              <Input type="text" placeholder="name@exemple.com" />
-              <Input type="password" placeholder="password" />
-              <Button>Sign In</Button>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  style={{
+                    width: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '24px',
+                    alignSelf: 'center',
+                  }}
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input placeholder="name@exemple.com" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button>Sign In</Button>
+                </form>
+              </Form>
             </InputsContainer>
           </FormContainer>
         </MainContainer>
