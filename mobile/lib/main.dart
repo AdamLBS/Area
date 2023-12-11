2a26ffa511b6ea6fd454de60c55d8c5610e21d6c
Adam Elaoumari <adam@admlbs.fr>
2023-12-11 17:48:17 +0100

    feat: Added bottom custom navbar for home/profile/settings pages

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:area/pages/home.dart';
 import 'package:area/pages/login.dart';
+import 'package:area/pages/settings.dart';
+import 'package:area/pages/user.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/register.dart';
+import 'utils/transition.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,6 +22,12 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF09090B)),
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android: CustomTransitionBuilder(),
+              TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+            },
+          ),
         ),
         home: LoginPage(),
         onGenerateRoute: (settings) {
@@ -29,6 +38,10 @@ class MyApp extends StatelessWidget {
               return MaterialPageRoute(builder: (context) => RegisterPage());
             case '/home':
               return MaterialPageRoute(builder: (context) => HomePage());
+            case '/profile':
+              return MaterialPageRoute(builder: (context) => UserPage());
+            case '/settings':
+              return MaterialPageRoute(builder: (context) => SettingsPage());
             default:
               return MaterialPageRoute(builder: (context) => LoginPage());
           }
---
+++ mobile/lib/utils/transition.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomTransitionBuilder extends PageTransitionsBuilder {
+  const CustomTransitionBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T>? route,
+    BuildContext? context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget? child,
+  ) {
+    return FadeTransition(opacity: animation, child: child);
+
+    // only return the child without warping it with animations
+  }
+}
