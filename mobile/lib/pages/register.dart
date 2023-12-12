b7d4a8a17cc01511778816af31a6a58a9864cee7
Luluri <lucas.rizzuto@epitech.eu>
2023-12-12 11:28:09 +0100

    feature(mobil): add register first and second page

--- mobile/lib/pages/register.dart
+++ mobile/lib/pages/register.dart
@@ -1,3 +1,5 @@
+import 'package:area/widgets/register_first_page.dart';
+import 'package:area/widgets/register_second_page.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,8 +10,14 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  int index = 1;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (index == 0)
+      return RegisterFirstPage();
+    else if (index == 1)
+      return RegisterSecondPage();
+    else
+      return Placeholder();
   }
 }
---
+++ mobile/lib/widgets/register_first_page.dart
@@ -0,0 +1,151 @@
+import 'package:area/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class RegisterFirstPage extends StatefulWidget {
+  const RegisterFirstPage({super.key});
+
+  @override
+  State<RegisterFirstPage> createState() => _RegisterFirstPageState();
+}
+
+class _RegisterFirstPageState extends State<RegisterFirstPage> {
+  final emailController = TextEditingController();
+  String errorMessage = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Color(0xFF09090B),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.only(right: 20, left: 20, bottom: 20, top: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset("assets/logos/icon.svg"),
+                Spacer(),
+                Text(
+                  "Sign Up",
+                  style: GoogleFonts.inter(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "Enter your account access below to sign up",
+                  style: GoogleFonts.inter(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFFA1A1AA),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  height: 40,
+                  child: TextFormField(
+                    controller: emailController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter an email";
+                      }
+                      final emailRegex = RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+                      if (!emailRegex.hasMatch(value)) {
+                        return "Please enter a valid email";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        labelText: "name@exemple.com",
+                        labelStyle: GoogleFonts.inter(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFFA1A1AA),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        errorStyle: TextStyle(height: 0),
+                        contentPadding: EdgeInsets.all(10.0)),
+                    style: GoogleFonts.inter(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                if (errorMessage != "") SizedBox(height: 8),
+                if (errorMessage != "")
+                  Text(
+                    errorMessage,
+                    style: GoogleFonts.inter(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.red,
+                    ),
+                  ),
+                Spacer(),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: purpleBackground,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    minimumSize: Size(double.infinity, 36),
+                  ),
+                  onPressed: () async {
+                    if (emailController.text.isEmpty) {
+                      setState(() {
+                        errorMessage = "Please fill the email fields";
+                      });
+                      return;
+                    } else {
+                      setState(() {
+                        errorMessage = "";
+                      });
+                    }
+                    // set request to api
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: GoogleFonts.inter(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF09090B),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      side: BorderSide(color: Color(0xFF27272A)),
+                    ),
+                    minimumSize: Size(double.infinity, 36),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed("/login");
+                  },
+                  child: Text(
+                    "I already have an account",
+                    style: GoogleFonts.inter(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
