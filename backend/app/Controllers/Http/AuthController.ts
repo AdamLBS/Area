30ccf0179594ada159c936412e62bbcf47a71e18
Florian Grima <florian.grima@epitech.eu>
2023-12-13 19:14:55 +0100

    fix(auth): fix ci/cd

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -1,5 +1,4 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import ApiToken from 'App/Models/ApiToken'
 import User from 'App/Models/User'
 import AuthValidator from 'App/Validators/User/AuthValidator'
 import RegisterValidator from 'App/Validators/User/RegisterValidator'
