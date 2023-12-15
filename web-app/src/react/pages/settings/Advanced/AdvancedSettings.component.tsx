99def942ccfc558c0c911898d5cbe318cfb78481
gabrielmorais4 <gabriel.de-souza-morais@epitech.eu>
2023-12-15 15:28:24 +0100

    feature(web-app): change web theme

--- web-app/src/react/pages/settings/Advanced/AdvancedSettings.component.tsx
+++ web-app/src/react/pages/settings/Advanced/AdvancedSettings.component.tsx
@@ -6,15 +6,23 @@ import {
   ThemeBody,
 } from './AdvancedSettings.style';
 import { PrimaryMutted, PrimarySmall } from '@/lib/ui/design-system';
+import { useTheme } from 'next-themes';
+
+//TODO: refactor the theme selection to use styled components
 
 const Advanced = () => {
+  const { setTheme } = useTheme();
+
   return (
     <PageContainer>
       <ThemeContainer>
         <PrimarySmall>Theme</PrimarySmall>
         <PrimaryMutted>Select your dashboard theme.</PrimaryMutted>
         <ThemeBody>
-          <div className="items-center rounded-md border-2 border-muted p-1 hover:bg-accent hover:text-accent-foreground">
+          <div
+            className="items-center rounded-md border-2 border-muted p-1 hover:bg-accent hover:text-accent-foreground"
+            onClick={() => setTheme('light')}
+          >
             <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
               <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
                 <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
@@ -30,7 +38,10 @@ const Advanced = () => {
               </div>
             </div>
           </div>
-          <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
+          <div
+            className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground"
+            onClick={() => setTheme('dark')}
+          >
             <div className="space-y-2 rounded-sm bg-slate-950 p-2">
               <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
                 <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
