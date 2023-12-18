4fd06624e24603e9446ce0dbc9d28f55270a991a
Ramdane Martin <martin.ramdane@epitech.eu>
2023-12-18 17:39:43 +0100

    feat(backend): remove web guard

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -9,8 +9,6 @@ export default class AuthController {
 
     const user = await User.firstOrCreate(payload)
 
-    await auth.use('web').login(user)
-
     const userToken = await auth.use('api').generate(user, {
       expiresIn: '7 days',
     })
