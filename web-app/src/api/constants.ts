b0e516548ec760b5768f73b5e7143d198257c0a2
Ramdane Martin <martin.ramdane@epitech.eu>
2023-12-20 10:42:27 +0100

    feat(WebApp): change interface for type

--- web-app/src/api/constants.ts
+++ web-app/src/api/constants.ts
@@ -5,8 +5,8 @@ export const API_URL_OAUTH =
 export type Token = {
   token: string;
 };
-export interface Service {
+export type Service = {
   provider: string;
-}
+};
 
 export type Services = Service[];
