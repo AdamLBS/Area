ef9d7bf67108bfbe81abf9947c69c3d982a6d0aa
gabrielmorais4 <gabriel.de-souza-morais@epitech.eu>
2024-01-03 16:01:59 +0100

    feat(web-app): about.json

---
+++ backend/app/Controllers/Http/AboutController.ts
@@ -0,0 +1,31 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AboutController {
+  public async info(ctx: HttpContextContract) {
+    return ctx.response.ok({
+      client: {
+        host: ctx.request.ip(),
+      },
+      server: {
+        current_time: Math.floor(new Date().getTime() / 1000),
+        services: [
+            {
+                name: "Spotify",
+                actions: [],
+                reactions: []
+            },
+            {
+                name: "Discord",
+                actions: [],
+                reactions: []
+            },
+            {
+                name: "Google",
+                actions: [],
+                reactions: []
+            }
+        ],
+      },
+    })
+  }
+}
