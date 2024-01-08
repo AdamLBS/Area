40bb832b2c0fc110893c8e24dfb7cdb593afa608
gabrielmorais4 <gabriel.de-souza-morais@epitech.eu>
2024-01-08 14:17:42 +0100

    feat(back): Refresh token generic

--- backend/app/Tasks/RefreshTokensTask.ts
+++ backend/app/Tasks/RefreshTokensTask.ts
@@ -1,54 +1,61 @@
-import { BaseTask, CronTimeV2 } from 'adonis5-scheduler/build/src/Scheduler/Task'
-import Database from '@ioc:Adonis/Lucid/Database'
-import axios from 'axios'
+import { BaseTask, CronTimeV2 } from 'adonis5-scheduler/build/src/Scheduler/Task';
+import Database from '@ioc:Adonis/Lucid/Database';
+import axios from 'axios';
 
 type RefreshToken = {
-  access_token: string
-  token_type: string
-  scope: string[] | string
-  expires_in: number
-  refresh_token: string
-}
+  access_token: string;
+  token_type: string;
+  scope: string[] | string;
+  expires_in: number;
+  refresh_token: string;
+};
 
 export default class RefreshTokensTask extends BaseTask {
   public static get schedule() {
-    console.log('[Refresh Token] schedule')
-    return CronTimeV2.everyFiveSeconds()
+    console.log('[Refresh Token] schedule');
+    return CronTimeV2.everyFifteenSeconds();
   }
 
   public static get useLock() {
-    return false
+    return false;
   }
 
-  private async refreshTwitchToken(oauth: any) {
+  private async refreshToken(oauth: any, provider: string, apiUrl: string, clientId?: string, clientSecret?: string) {
     try {
       const params = {
         grant_type: 'refresh_token',
         refresh_token: oauth.refresh_token,
-        client_id: process.env.TWITCH_CLIENT_ID,
-        client_secret: process.env.TWITCH_CLIENT_SECRET,
-      }
+        client_id: clientId,
+        client_secret: clientSecret,
+      };
+
       const response = await axios.post<RefreshToken>(
-        `https://id.twitch.tv/oauth2/token?grant_type=${params.grant_type}&refresh_token=${params.refresh_token}&client_id=${params.client_id}&client_secret=${params.client_secret}`
-        )
+        `${apiUrl}?grant_type=${params.grant_type}&refresh_token=${params.refresh_token}&client_id=${params.client_id}&client_secret=${params.client_secret}`
+      );
       await Database.from('oauths')
-        .where('provider', 'twitch')
+        .where('provider', provider)
         .where('user_uuid', oauth.user_uuid)
-        .update({ token: response.data.access_token, refresh_token: response.data.refresh_token })
+        .update({ token: response.data.access_token, refresh_token: response.data.refresh_token });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
   public async handle() {
     try {
-      const oauths = await Database.from('oauths')
+      const oauths = await Database.from('oauths');
+      var num = 0;
       for (const oauth of oauths) {
-        if (oauth.provider === 'twitch')
-          await this.refreshTwitchToken(oauth)
+        num++;
+        console.log(`[Refresh Token] ${num}`);
+        if (oauth.provider === 'twitch') {
+          await this.refreshToken(oauth, 'twitch', 'https://id.twitch.tv/oauth2/token', process.env.TWITCH_CLIENT_ID, process.env.TWITCH_CLIENT_SECRET);
+        } else if (oauth.provider === 'spotify') {
+          await this.refreshToken(oauth, 'spotify', 'https://accounts.spotify.com/api/token', process.env.SPOTIFY_CLIENT_ID, process.env.SPOTIFY_CLIENT_SECRET);
+        }
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 }
