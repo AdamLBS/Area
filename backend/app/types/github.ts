0442eaf2884f52bc2e40d10f0b85b32dc613dbe4
Florian Grima <florian.grima@epitech.eu>
2024-01-08 14:15:09 +0100

    feat(backend): check last commit in github repository

---
+++ backend/app/types/github.ts
@@ -0,0 +1,15 @@
+export type GithubUser = {
+  login: string
+  id: number
+  repos_url: string
+}
+
+export type Repository = {
+  id: number
+  name: string
+  commits_url: string
+}
+
+export type Commit = {
+  sha: string
+}
