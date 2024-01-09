b469330fa924e78ebe852ddd8df85904657e4444
Adam Elaoumari <adam@admlbs.fr>
2024-01-09 15:48:05 +0100

    feat & fix: added cache for spotify like task

--- backend/app/Tasks/SpotifyLikeSong.ts
+++ backend/app/Tasks/SpotifyLikeSong.ts
@@ -3,6 +3,8 @@ import { BaseTask, CronTimeV2 } from 'adonis5-scheduler/build/src/Scheduler/Task
 import { eventHandler, ResponseInteraction } from 'App/functions/EventHandler'
 import axios from 'axios'
 import { APIEventField } from 'types/events'
+import Cache from 'App/Models/Cache'
+
 
 export interface Artist {
   name: string
@@ -20,12 +22,10 @@ type SpotifyLikesSong = {
   items: Item[]
 }
 
-let globalSpotifyListeners: SpotifyLikesSong = { total: -1, items: [] }
-
 export default class SpotifyLikeSong extends BaseTask {
   public static get schedule() {
     // Use CronTimeV2 generator:
-    return CronTimeV2.everySecond()
+    return CronTimeV2.everyFiveSeconds()
     // or just use return cron-style string (simple cron editor: crontab.guru)
   }
   /**
@@ -49,51 +49,62 @@ export default class SpotifyLikeSong extends BaseTask {
     }
   }
 
+  private async updateNumberOfLikedSongs(uuid: any, songs: number) {
+    console.log("updating to " + songs);
+    await Cache.updateOrCreate(
+      {
+        uuid: uuid,
+      },
+      {
+        spotifyLikedSongs: songs
+      }
+    )
+  }
+
   public async handle() {
     const events = await Database.query()
       .from('events')
       .whereRaw(`CAST(trigger_interaction AS JSONB) #>> '{id}' = 'likeSong'`)
-    for (const event of events) {
-      console.log("events size :" + events.length)
       const triggerApi = await Database.query()
         .from('oauths')
-        .where('uuid', event.trigger_api)
+        .where('uuid', events.at(0).trigger_api)
         .first()
-      if (triggerApi && triggerApi.token) {
-        const spotifyLikesSong = await this.fetchSpotifyData(triggerApi.token)
-        if (globalSpotifyListeners.total === -1) {
-          globalSpotifyListeners = spotifyLikesSong
-        } else if (globalSpotifyListeners.total < spotifyLikesSong.total) {
-          const jsonVals = JSON.parse(event.response_interaction)
-          const responseInteraction = jsonVals.id.toString() as ResponseInteraction
-          const fields = jsonVals.fields as APIEventField<any>[]
-          for (const field of fields) {
-            if ((field.value as string).includes('$artist')) {
-              let replaceValue = ''
-              for (const artist of spotifyLikesSong.items[0].track.artists) {
-                replaceValue += artist.name
-                if (
-                  spotifyLikesSong.items[0].track.artists.length === 2 &&
-                  artist === spotifyLikesSong.items[0].track.artists[0]
-                )
-                  replaceValue += ' et '
-                else if (
-                  spotifyLikesSong.items[0].track.artists.indexOf(artist) !==
-                  spotifyLikesSong.items[0].track.artists.length - 1
-                )
-                  replaceValue += ', '
+      const spotifyLikesSong = await this.fetchSpotifyData(triggerApi.token)
+      const userCache = await Database.query().from('caches').where('uuid', triggerApi.user_uuid).first();
+      for (const event of events) {
+        if (triggerApi && triggerApi.token) {
+          if (!userCache || !userCache?.spotify_liked_songs) {
+            await this.updateNumberOfLikedSongs(triggerApi.user_uuid, spotifyLikesSong.total);
+          } else if (userCache.spotify_liked_songs < spotifyLikesSong.total) {
+            const jsonVals = JSON.parse(event.response_interaction)
+            const responseInteraction = jsonVals.id.toString() as ResponseInteraction
+            const fields = jsonVals.fields as APIEventField<any>[]
+            for (const field of fields) {
+              if ((field.value as string).includes('$artist')) {
+                let replaceValue = ''
+                for (const artist of spotifyLikesSong.items[0].track.artists) {
+                  replaceValue += artist.name
+                  if (
+                    spotifyLikesSong.items[0].track.artists.length === 2 &&
+                    artist === spotifyLikesSong.items[0].track.artists[0]
+                  )
+                    replaceValue += ' et '
+                  else if (
+                    spotifyLikesSong.items[0].track.artists.indexOf(artist) !==
+                    spotifyLikesSong.items[0].track.artists.length - 1
+                  )
+                    replaceValue += ', '
+                }
+                field.value = field.value.replace('$artist', replaceValue)
               }
-              field.value = field.value.replace('$artist', replaceValue)
+              if ((field.value as string).includes('$song'))
+                field.value = field.value.replace('$song', spotifyLikesSong.items[0].track.name)
             }
-            if ((field.value as string).includes('$song'))
-              field.value = field.value.replace('$song', spotifyLikesSong.items[0].track.name)
+            await eventHandler(responseInteraction, fields, event.response_api)
           }
-          await eventHandler(responseInteraction, fields, event.response_api)
-          globalSpotifyListeners = spotifyLikesSong;
-        } else {
-          globalSpotifyListeners = spotifyLikesSong
-        }
       }
     }
+    if (spotifyLikesSong != userCache?.spotify_liked_songs)
+      await this.updateNumberOfLikedSongs(triggerApi.user_uuid, spotifyLikesSong.total);
   }
 }
