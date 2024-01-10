35e4e83f281f582095c0e58f482f4b81ca2728d8
Adam Elaoumari <adam@admlbs.fr>
2024-01-10 18:59:49 +0100

    feat: Implemented additionnal actions to Github & Twitch tasks

--- backend/app/Tasks/GithubNewCommitTask.ts
+++ backend/app/Tasks/GithubNewCommitTask.ts
@@ -1,6 +1,6 @@
 import Database from '@ioc:Adonis/Lucid/Database'
 import Oauth from 'App/Models/Oauth'
-import { ResponseInteraction, eventHandler } from 'App/functions/EventHandler'
+import { ResponseInteraction, eventHandler, handleAdditionalActions } from 'App/functions/EventHandler'
 import { BaseTask, CronTimeV2 } from 'adonis5-scheduler/build/src/Scheduler/Task'
 import axios from 'axios'
 import { APIEventField } from 'types/events'
@@ -10,7 +10,7 @@ import { Commit } from 'App/types/github'
 export default class GithubCheckLastCommitTask extends BaseTask {
   public static get schedule() {
     // Use CronTimeV2 generator:
-    return CronTimeV2.everySecond()
+    return CronTimeV2.everyThirtySeconds()
     // or just use return cron-style string (simple cron editor: crontab.guru)
   }
 
@@ -32,14 +32,30 @@ export default class GithubCheckLastCommitTask extends BaseTask {
       console.error(error)
     }
   }
+  private  useVariablesInFields  = (
+    fields: APIEventField<any>[],
+    response: Commit
+  ) => {
+    for (const field of fields) {
+      if ((field.value as string).includes('$commitAuthor')) {
+        let commitAuthor = response.commit.author.name
+        field.value = field.value.replace('$commitAuthor', commitAuthor)
+      }
+      if ((field.value as string).includes('$commitMsg')) {
+        let commitMessage = response.commit.message
+        field.value = field.value.replace('$commitMsg', commitMessage)
+      }
+      if ((field.value as string).includes('$commitUrl')) {
+        let commitUrl = response.html_url
+        field.value = field.value.replace('$commitUrl', commitUrl)
+      }
+    }
+  }
 
   private async fetchLastCommit(
     repositoryUrl: string,
     oauth: Oauth,
-    eventUuid: string,
-    responseApiUuid: string,
-    reponseInteraction: ResponseInteraction,
-    responseFields: APIEventField<string>[]
+    event: any,
   ) {
     const commitsUrl = repositoryUrl.replace('github.com', 'api.github.com/repos')
     try {
@@ -52,26 +68,23 @@ export default class GithubCheckLastCommitTask extends BaseTask {
           },
         })
       ).data[0] as Commit
-      const userCache = await Cache.query().from('caches').where('uuid', eventUuid).first()
+      const userCache = await Cache.query().from('caches').where('uuid', event.uuid).first()
       if (!userCache || !userCache.lastCommit) {
-        await this.updateLatestCommitId(eventUuid, response.sha)
+        await this.updateLatestCommitId(event.uuid, response.sha)
       } else if (userCache.lastCommit !== response.sha) {
-        await this.updateLatestCommitId(eventUuid, response.sha)
-        for (const field of responseFields) {
-          if ((field.value as string).includes('$commitAuthor')) {
-            let commitAuthor = response.commit.author.name
-            field.value = field.value.replace('$commitAuthor', commitAuthor)
-          }
-          if ((field.value as string).includes('$commitMsg')) {
-            let commitMessage = response.commit.message
-            field.value = field.value.replace('$commitMsg', commitMessage)
-          }
-          if ((field.value as string).includes('$commitUrl')) {
-            let commitUrl = response.html_url
-            field.value = field.value.replace('$commitUrl', commitUrl)
-          }
+        await this.updateLatestCommitId(event.uuid, response.sha)
+        const jsonVals = JSON.parse(event.response_interaction)
+        const responseInteraction = jsonVals.id.toString() as ResponseInteraction
+        const fields = jsonVals.fields as APIEventField<any>[]
+        this.useVariablesInFields(fields, response)
+        for (const additionalAction of event.additional_actions) {
+          this.useVariablesInFields(
+            additionalAction.fields, response
+          )
+          console.log("updating additionnal")
         }
-        await eventHandler(reponseInteraction, responseFields, responseApiUuid)
+        await eventHandler(responseInteraction, fields, event.response_api)
+        await handleAdditionalActions(event)
       }
     } catch (error: any) {
       console.error(error)
@@ -93,16 +106,10 @@ export default class GithubCheckLastCommitTask extends BaseTask {
           try {
             const jsonVals = JSON.parse(event.trigger_interaction)
             const fields = jsonVals.fields as APIEventField<string>[]
-            const jsonResponse = JSON.parse(event.response_interaction)
-            const responseFields = jsonResponse.fields as APIEventField<string>[]
-            const responseInteraction = jsonResponse.id.toString() as ResponseInteraction
             await this.fetchLastCommit(
               fields[0].value,
               oauth,
-              event.uuid,
-              event.response_api,
-              responseInteraction,
-              responseFields
+              event,
             )
           } catch (error: any) {
             console.error(error)
