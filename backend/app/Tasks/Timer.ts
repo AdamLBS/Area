76f300b2072efb4c060a54eb317016d27937efe3
Ramdane Martin <martin.ramdane@epitech.eu>
2024-01-10 10:27:56 +0100

    feat(backend):added timer task

--- backend/app/Tasks/Timer.ts
+++ backend/app/Tasks/Timer.ts
@@ -1,4 +1,5 @@
 import Database from '@ioc:Adonis/Lucid/Database'
+import Cache from 'App/Models/Cache'
 import { ResponseInteraction, eventHandler } from 'App/functions/EventHandler'
 import { BaseTask, CronTimeV2 } from 'adonis5-scheduler/build/src/Scheduler/Task'
 import { APIEventField } from 'types/events'
@@ -6,7 +7,7 @@ import { APIEventField } from 'types/events'
 export default class Timer extends BaseTask {
   public static get schedule() {
     // Use CronTimeV2 generator:
-    return CronTimeV2.everySecond()
+    return CronTimeV2.everyFiveSeconds()
     // or just use return cron-style string (simple cron editor: crontab.guru)
   }
   /**
@@ -17,22 +18,42 @@ export default class Timer extends BaseTask {
     return false
   }
 
+  private async updateLastTimerActive(uuid: string, active: boolean) {
+    await Cache.updateOrCreate(
+      {
+        uuid: uuid,
+      },
+      {
+        timerActive: active,
+      }
+    )
+  }
+
   public async handle() {
     this.logger.info('Timer Handled')
     const events = await Database.query()
       .from('events')
       .whereRaw(`CAST(trigger_interaction AS JSONB) #>> '{id}' = 'everyDayTimer'`)
     for (const event of events) {
-      const jsonValsResponse = JSON.parse(event.response_interaction)
       const jsonValsAction = JSON.parse(event.trigger_interaction)
-      const responseInteraction = jsonValsResponse.id.toString() as ResponseInteraction
       const fields = jsonValsAction.fields as APIEventField<any>[]
       const time = fields[0].value
       const now = new Date()
       const [hours, minutes] = time.split(':').map(Number)
+      const userCache = await Cache.query().from('caches').where('uuid', event.uuid).first()
+      if (!userCache) {
+        await this.updateLastTimerActive(event.uuid, false)
+      }
       if (now.getHours() === hours && now.getMinutes() === minutes) {
-        console.log('Timer triggered')
-        await eventHandler(responseInteraction, [], event.response_api)
+        if (userCache && !userCache.timerActive) {
+          const jsonVals = JSON.parse(event.response_interaction)
+          const responseInteraction = jsonVals.id.toString() as ResponseInteraction
+          const fields = jsonVals.fields as APIEventField<any>[]
+          await eventHandler(responseInteraction, fields, event.response_api)
+          await this.updateLastTimerActive(event.uuid, true)
+        }
+      } else if (userCache && userCache.timerActive) {
+        await this.updateLastTimerActive(event.uuid, false)
       }
     }
   }
