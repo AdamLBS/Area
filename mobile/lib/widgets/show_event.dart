0a52cb90a45fa98598c514517ace5d8b4998a33e
Adam <adam@admlbs.fr>
2024-01-11 11:54:11 +0100

    feat: added Event card when clicking an event on the list

--- mobile/lib/widgets/show_event.dart
+++ mobile/lib/widgets/show_event.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:area/model/event_create_model.dart';
+import 'package:area/widgets/event_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,9 +17,9 @@ class ShowEvent extends StatefulWidget {
 class _ShowEventState extends State<ShowEvent> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
+    print(widget.event.responseEvent!.name);
+    return ListView(
+      shrinkWrap: true,
       children: [
         Container(
           constraints: BoxConstraints(minHeight: 102, minWidth: 320),
@@ -29,26 +30,31 @@ class _ShowEventState extends State<ShowEvent> {
             borderRadius: BorderRadius.circular(10),
           ),
           padding: EdgeInsets.all(24),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Text(
-                  widget.event.eventName!,
-                  style: GoogleFonts.inter(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white),
-                ),
-                Text(
-                  widget.event.eventDescription!,
-                  style: GoogleFonts.inter(fontSize: 14, color: Colors.white),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Text(
+                      widget.event.eventName!,
+                      style: GoogleFonts.inter(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      widget.event.eventDescription!,
+                      style: GoogleFonts.inter(fontSize: 14, color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+              
+                  ]),
+                                  Container(
                   width: 42,
                   height: 42,
                   padding: EdgeInsets.all(4),
@@ -69,8 +75,50 @@ class _ShowEventState extends State<ShowEvent> {
                     ),
                   ),
                 )
-              ]),
-        )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Divider(
+          color: Color(0xFFFFFFFF),
+          thickness: 0.1,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "Trigger event",
+          style: GoogleFonts.inter(
+              fontWeight: FontWeight.w500, fontSize: 16, color: Colors.white),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        EventCard(
+          desc: widget.event.triggerEvent!.name,
+          name: widget.event.triggerEvent!.provider,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Divider(
+          color: Color(0xFFFFFFFF),
+          thickness: 0.1,
+        ),
+        Text(
+          "Action event",
+          style: GoogleFonts.inter(
+              fontWeight: FontWeight.w500, fontSize: 16, color: Colors.white),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        EventCard(
+          desc: widget.event.responseEvent!.name,
+          name: widget.event.responseEvent!.provider,
+        ),
       ],
     );
   }
