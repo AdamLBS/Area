7977148621fc461588c2784d69b3c3c65c202667
Adam <adam@admlbs.fr>
2024-01-14 18:55:15 +0100

    fix: removed ssl from docker config

--- backend/server.ts
+++ backend/server.ts
@@ -12,22 +12,8 @@
 
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
-import { createServer } from 'https'
-
 import { Ignitor } from '@adonisjs/core/build/standalone'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
-if (process.env.SSL_KEY !== undefined && process.env.SSL_KEY !== '') {
-  new Ignitor(__dirname).httpServer().start((handle) => {
-    return createServer(
-      {
-        key: process.env.SSL_KEY_PATH,
-        cert: process.env.SSL_CERT_PATH,
-      },
-      handle
-    )
-  })
-} else {
-  new Ignitor(__dirname).httpServer().start()
-}
+new Ignitor(__dirname).httpServer().start()
