5964d3562c7c7761f6c0cd9b5b15bb34b4273e12
Adam <adam@admlbs.fr>
2024-01-14 18:37:33 +0100

    feat: added ssl for backend

--- backend/server.ts
+++ backend/server.ts
@@ -12,8 +12,19 @@
 
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
+import { createServer } from "https";
+
 import { Ignitor } from '@adonisjs/core/build/standalone'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
-new Ignitor(__dirname).httpServer().start()
+if (process.env.SSL_KEY !== undefined && process.env.SSL_KEY !== "") {
+    new Ignitor(__dirname).httpServer().start((handle) => {
+        return createServer({
+            key: process.env.SSL_KEY,
+            cert: process.env.SSL_CERT
+        }, handle)
+    });
+} else {
+    new Ignitor(__dirname).httpServer().start();
+}
