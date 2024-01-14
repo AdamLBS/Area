0453b2507451e841b5745f013dda2c40bf3d947d
Adam <adam@admlbs.fr>
2024-01-14 20:01:42 +0100

    feat: added script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-##Pulling and checking changes from git
+# Check if the current directory is a Git repository
+if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+  echo "Not a Git repository. Exiting."
+  exit 1
+fi
 
+# Check if there are any changes
+if git diff --quiet; then
+  echo "No changes in the repository."
+else
+  echo "Changes detected in the repository. Stopping Docker containers, pulling changes, and restarting containers."
+
+  # Stop all running Docker containers
+  docker stop $(docker ps -q)
+
+  # Perform a git pull
+  git pull
+
+  # Bring up Docker services using docker-compose
+  docker-compose up -d --build --force-recreate
+fi
