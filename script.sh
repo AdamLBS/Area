d59899bd1a6a7c74a6924a07ed39c91d69f0fe26
Adam Elaoumari <adam@admlbs.fr>
2024-01-15 13:29:27 +0100

    fix: added reset of docker when rebuilding containers

--- script.sh
+++ script.sh
@@ -8,7 +8,8 @@ else
 
   # Stop all running Docker containers
   docker stop $(docker ps -q)
-
+  docker volume prune --force
+  docker rmi -f $(docker images -aq)
   # Bring up Docker services using docker-compose
   docker-compose up -d --build --force-recreate
 fi
